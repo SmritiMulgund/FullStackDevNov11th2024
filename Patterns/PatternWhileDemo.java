45d85c685473d5600c923e3f273c59268489379e
SmritiMulgund <mulgundsmriti@gmail.com>
2025-01-16 12:43:56 +0530

    Inital Draft

---
+++ Patterns/PatternWhileDemo.java
@@ -0,0 +1,18 @@
+class PatternWhileDemo
+{
+	public static void main(String args[])
+	{
+		int i=1;
+		while(i<=3)
+		{
+			int j=1;
+			while(j<=5)
+			{
+				System.out.print("# ");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
